b86d531643997276c4ba32df5145520af16baaf2
Tyler Kindy <tylerkindy@gmail.com>
2024-12-03 07:53:41 -0500

    Complete day 3, part 2

--- src/com/tylerkindy/aoc2024/day03.clj
+++ src/com/tylerkindy/aoc2024/day03.clj
@@ -1,4 +1,6 @@
-(ns com.tylerkindy.aoc2024.day03)
+(ns com.tylerkindy.aoc2024.day03
+  (:require [clojure.core.match :refer [match]]
+            [clojure.string :as str]))
 
 (defn find-muls [memory]
   (->> memory
@@ -12,6 +14,35 @@
        (map (partial apply *))
        (apply +)))
 
+(defn parse-instructions [memory]
+  (->> memory
+       (re-seq #"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+       (map (fn [[instruction & args]]
+              (cond
+                (str/starts-with? instruction "mul")
+                (->> args
+                     (map parse-long)
+                     (concat [:mul])
+                     (into []))
+
+                (str/starts-with? instruction "don't")
+                [:dont]
+
+                :else [:do])))))
+
+(defn part2 [memory]
+  (->> memory
+       parse-instructions
+       (reduce (fn [[acc on] [instruction & args]]
+                 (match [instruction on]
+                   [:mul true] [(+ acc (apply * args)) on]
+                   [:do _] [acc true]
+                   [:dont _] [acc false]
+                   :else [acc on]))
+               [0 true])
+       first))
+
 (defn -main []
   (let [memory (slurp "input/day03.txt")]
-    (println "Part 1:" (part1 memory))))
+    (println "Part 1:" (part1 memory))
+    (println "Part 2:" (part2 memory))))
--- test/com/tylerkindy/aoc2024/day03_test.clj
+++ test/com/tylerkindy/aoc2024/day03_test.clj
@@ -11,3 +11,18 @@
 (deftest part1
   (is (= (day03/part1 example)
          161)))
+
+(def example2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+(deftest parse-instructions
+  (is (= (day03/parse-instructions example2)
+         (list [:mul 2 4]
+               [:dont]
+               [:mul 5 5]
+               [:mul 11 8]
+               [:do]
+               [:mul 8 5]))))
+
+(deftest part2
+  (is (= (day03/part2 example2)
+         48)))
