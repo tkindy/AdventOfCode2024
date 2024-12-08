07b3fb26055d628d8c007f113b3e58b55c058497
Tyler Kindy <tylerkindy@gmail.com>
2024-12-08 00:44:56 -0500

    [Day 08] Complete part 1

--- src/com/tylerkindy/aoc2024/day08.clj
+++ src/com/tylerkindy/aoc2024/day08.clj
@@ -1,5 +1,7 @@
 (ns com.tylerkindy.aoc2024.day08
-  (:require [clojure.string :as str]))
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
+            [com.tylerkindy.aoc2024.util.vector :as vec]))
 
 (defn parse-input [input]
   (let [lines (str/split-lines input)]
@@ -22,5 +24,40 @@
                                        antennas)))
                         {}))}))
 
+(defn all-pairs [antennas]
+  (if (< (count antennas) 2)
+    #{}
+    (letfn [(help [l rs]
+              (->> rs
+                   (map (fn [r] #{l r}))
+                   (into #{})))]
+      (set/union (help (first antennas) (rest antennas))
+                 (all-pairs (rest antennas))))))
+
+(defn antinodes [pair]
+  (let [l (first pair)
+        r (second pair)
+        v (vec/- r l)]
+    #{(vec/- l v)
+      (vec/+ l (vec/* v 2))}))
+
+(defn in-bounds? [[x y] [width height]]
+  (and (>= x 0)
+       (< x width)
+       (>= y 0)
+       (< y height)))
+
+(defn unique-antinodes [{:keys [dimensions antennas]}]
+  (->> antennas
+       vals
+       (mapcat all-pairs)
+       (mapcat antinodes)
+       (filter #(in-bounds? % dimensions))
+       (into #{})))
+
+(defn part1 [world]
+  (count (unique-antinodes world)))
+
 (defn -main []
-  (let [world (parse-input (slurp "input/day08.txt"))]))
+  (let [world (parse-input (slurp "input/day08.txt"))]
+    (println "Part 1:" (part1 world))))
--- test/com/tylerkindy/aoc2024/day08_test.clj
+++ test/com/tylerkindy/aoc2024/day08_test.clj
@@ -22,3 +22,26 @@
 (deftest parse-input
   (is (= (day08/parse-input example-input)
          example)))
+
+(deftest all-pairs
+  (is (= (day08/all-pairs #{[8 1] [5 2] [7 3] [4 4]})
+         #{#{[8 1] [5 2]} #{[8 1] [7 3]} #{[8 1] [4 4]}
+           #{[5 2] [7 3]} #{[5 2] [4 4]}
+           #{[7 3] [4 4]}})))
+
+(deftest unique-antinodes
+  (is (= (day08/unique-antinodes example)
+         #{[6 0] [11 0]
+           [3 1]
+           [4 2] [10 2]
+           [2 3]
+           [9 4]
+           [1 5] [6 5]
+           [3 6]
+           [0 7] [7 7]
+           [10 10]
+           [10 11]})))
+
+(deftest part1
+  (is (= (day08/part1 example)
+         14)))
